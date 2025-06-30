0c91fdb40d6468008fc22734a38a859ed414a929
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2025-06-30 13:39:59 +0530

    Add authentication, user, driver, and vehicle modules with controllers and services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DriverModule } from './driver/driver.module';
+import { VehicleModule } from './vehicle/vehicle.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, DriverModule, VehicleModule],
   controllers: [AppController],
   providers: [AppService],
 })
