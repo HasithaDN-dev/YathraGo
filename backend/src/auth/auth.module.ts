aec64e2a5dd3288dc4dff659bf0b1368fb8de43d
Thisum Dinujaya <140166006+ThisumDinujaya@users.noreply.github.com>
2025-07-09 18:48:26 +0530

    Remove PrismaController and jwt default key

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -15,7 +15,7 @@ import { CommonModule } from '../common/common.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET', 'your-default-secret-key'),
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
           expiresIn: configService.get<string>('JWT_EXPIRES_IN', '7d'),
         },
