0b8881eb184fb98536fe6543f84930a3edd50e1e
Sineth Hansaja <sgamage149@gmail.com>
2025-07-11 22:25:55 +0530

    created role based protected routes

--- backend/src/vehicle/vehicle.controller.ts
+++ backend/src/vehicle/vehicle.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { VehicleService } from './vehicle.service';
+import { User } from 'src/auth-web/decorators';
+import {  Webuser } from '@prisma/client';
+import { JwtGuard } from 'src/auth-web/guards';
+import { RolesGuard } from 'src/auth-web/guards/roles.guards';
+import { Roles } from 'src/auth-web/decorators/roles.decorator';
 
-@Controller('vehicle')
-export class VehicleController {}
+@Controller()
+export class VehicleController {
+
+    constructor(private vehicleService: VehicleService) {}
+
+        @UseGuards(JwtGuard,RolesGuard)
+        @Get('owner/vehicles')
+        @Roles('owner')
+        getVehicles(@User() user:Webuser){
+            return this.vehicleService.getVehicles(user.id);
+           //return req.user
+        }
+
+    
+
+
+
+  
+}
--- backend/src/vehicle/vehicle.service.ts
+++ backend/src/vehicle/vehicle.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class VehicleService {}
+export class VehicleService {
+
+    constructor(private prisma: PrismaService) {}
+        getVehicles(userID: number): string {
+            return `List of vehicles of the user: ${userID}`;
+        }
+    
+}
