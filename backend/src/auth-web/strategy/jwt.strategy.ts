180da36547eae9c47844e5a4f633ee953530f8e5
Sineth Hansaja <sgamage149@gmail.com>
2025-07-11 12:13:26 +0530

    Fixing Typos

--- backend/src/auth-web/strategy/jwt.strategy.ts
+++ backend/src/auth-web/strategy/jwt.strategy.ts
@@ -6,7 +6,7 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private config: ConfigService) {
-    const secret = config.get<string>('WEB_JTW_SECRET');
+    const secret = config.get<string>('WEB_JWT_SECRET');
     if (!secret) {
       throw new Error('WEB_JWT_SECRET is not defined in .env');
     }
