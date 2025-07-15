7a0d3951b2c79bc26c7ec4bd8e08512a8133b580
K.P.H. De Silva <121782681+PasindhuHeshan@users.noreply.github.com>
2025-07-15 23:39:58 +0530

    Started on home page

--- web-dashboard/app/layout.tsx
+++ web-dashboard/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink
+} from "@/components/ui/navigation-menu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +32,38 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavigationMenu className="bg-light-gray text-deep-navy w-full p-4">
+          <div className="flex justify-between items-center w-full">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/">Icon</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/student">Student</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/office-staff">Office Staff</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/driver">Driver</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/owner">Owner</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/admin">Admin</NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+            <NavigationMenuList className="ml-auto">
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/login">Login</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink href="/sign-in">Sign In</NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </div>
+        </NavigationMenu>
         {children}
       </body>
     </html>
