b83b6ba6926939fde5ccf3a62e496959f53610bc
M.A.H.D. NAVARATHNE <140165356+Hasitha12389@users.noreply.github.com>
2025-07-16 05:31:44 +0530

    one update of layouts

--- mobile-customer/app/(registration)/_layout.tsx
+++ mobile-customer/app/(registration)/_layout.tsx
@@ -7,10 +7,6 @@ export default function RegistrationLayout() {
       <Stack.Screen name="registration-type" />
       <Stack.Screen name="staff-passenger" />
       <Stack.Screen name="child-registration" />
-      <Stack.Screen name="staff-step1" />
-      <Stack.Screen name="staff-step2" />
-      <Stack.Screen name="child-step1" />
-      <Stack.Screen name="child-step2" />
     </Stack>
   );
 }
